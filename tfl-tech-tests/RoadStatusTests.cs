a35f34bf08daa3cc7e6f5f19162574969f56f241
David North <bigchimp2005+github@gmail.com>
2019-01-12 14:18:40 +0000

    Road status can now return a formatted string of data

---
+++ tfl-tech-tests/RoadStatusTests.cs
@@ -0,0 +1,24 @@
+﻿
+using NUnit.Framework;
+using tfl_tech.Models;
+
+namespace Tests
+{
+    class RoadStatusTests
+    {
+        [Test]
+        public void TestFormattedText()
+        {
+            RoadStatus status = new RoadStatus() {
+                displayName = "<DISPLAYNAME>",
+                statusSeverity = "<STATUS>",
+                statusSeverityDescription = "<DESCRIPTION>"
+            };
+
+            Assert.AreEqual(
+                "The status of the <DISPLAYNAME> is as follows\r\n\tRoad Status is <STATUS>\r\n\tRoad Status Description is <DESCRIPTION>",
+                status.ToFormattedString()
+            );
+        }
+    }
+}
--- tfl-tech/Models/RoadStatus.cs
+++ tfl-tech/Models/RoadStatus.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Text;
+
 namespace tfl_tech.Models
 {
     public struct RoadStatus
@@ -6,5 +8,15 @@ namespace tfl_tech.Models
         public string displayName;
         public string statusSeverity;
         public string statusSeverityDescription;
+
+        public string ToFormattedString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.AppendFormat("The status of the {0} is as follows\r\n", displayName);
+            str.AppendFormat("\tRoad Status is {0}\r\n", statusSeverity);
+            str.AppendFormat("\tRoad Status Description is {0}", statusSeverityDescription);
+
+            return str.ToString();
+        }
     }
 }
