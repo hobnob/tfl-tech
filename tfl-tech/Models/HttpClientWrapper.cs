8a6ca3fe9e7260771738585d8d95f80415b4ffd6
David North <bigchimp2005+github@gmail.com>
2019-01-12 13:47:20 +0000

    Adds a client wrapper for the HttpClient

---
+++ tfl-tech/Models/HttpClientWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+
+namespace tfl_tech.Models
+{
+    public class HttpClientWrapper : IHttpClient
+    {
+        public Uri BaseAddress {
+            get {
+                return httpClient.BaseAddress;
+            }
+
+            set {
+                httpClient.BaseAddress = value;
+            }
+        }
+
+        private HttpClient httpClient;
+
+        public HttpClientWrapper(HttpClient initclient)
+        {
+            if (initclient == null) {
+                throw new ArgumentNullException("HttpClient cannot be null");
+            }
+
+            httpClient = initclient;
+        }
+
+        public HttpResponseMessage Get(string uri)
+        {
+            return httpClient.GetAsync(uri).Result;
+        }
+    }
+}
